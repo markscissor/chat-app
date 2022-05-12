80e47c18489aa0298e4f913583c610ea29fbfafc
markscissor <mnmunoz@up.edu.ph>
2022-05-12 14:37:41 +0400

    commit2

---
+++ .platform/hooks/postdeploy/x_restart_services.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# This file will make sure to restart on specific services like NGINX.
+# During postdeploy, we make sure to restart the services to take in consideration the new custom configs.
+# Please see the related issue(s): https://github.com/rennokki/laravel-aws-eb/issues/55
+
+# Feel free to uncomment the following lines in case you have issues. ¯\_(ツ)_/¯
+
+# fetching env properties and set to 
+export $(/opt/elasticbeanstalk/bin/get-config --output YAML environment | sed -r 's/: /=/' | xargs)
+
+sudo systemctl restart php-fpm.service
+sudo systemctl restart nginx.service
